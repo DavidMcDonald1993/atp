baa7ba751b7be7306f624043989bf3ecde3148d4
davemcdonald@live.co.uk <davemcdonald@live.co.uk>
2020-06-08 11:11:20 +0100

    fix

--- bash/synthetic.sh
+++ bash/synthetic.sh
@@ -29,7 +29,7 @@ if [ $exp == "recon_experiment" ]
 then 
     edgelist=${data_dir}/edgelist.tsv.gz
 else
-    edgelist=$(printf ../HEADNET/edgelists/${dataset}/seed=%03d/training_edges/edgelist.tsv ${seed})
+    edgelist=$(printf ../HEADNET/edgelists/${dataset}/seed=%03d/training_edges/edgelist.tsv.gz ${seed})
 fi
 echo edgelist is $edgelist
 
